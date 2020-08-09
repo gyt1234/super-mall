0ee565c4a850f12feed63c3e260cb673368c2598
gyt <2509900352@qq.com>
2020-08-09 22:32:00 +0800

    完成导航栏和轮播图组件的封装

---
+++ src/components/common/swiper/Swiper.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="block">
+    <el-carousel trigger="click" height="150px">
+      <el-carousel-item v-for="(list,index) in lists" :key="index">
+        <a :href="list.link"><img :src="list.image"></a>
+      </el-carousel-item>
+    </el-carousel>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Swiper',
+  props: {
+    lists: Array
+  }
+}
+</script>
+
+<style scoped>
+  .el-carousel__item img {
+    margin: 0;
+    height: 150px;
+    width: 100%;
+  }
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,46 @@
 <template>
-    <h2>首页</h2>
+    <div id="home">
+      <!-- 导航 -->
+      <nav-bar class="home-nav">
+        <template v-slot:center>购物街</template>
+      </nav-bar>
+      <!-- 轮播图 -->
+      <swiper :lists="banners"></swiper>
+    </div>
 </template>
 
 <script>
+import NavBar from '@/components/common/navbar/NavBar'
+import { getHomeMultidata } from '@/network/home'
+import Swiper from '@/components/common/swiper/Swiper'
+
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    NavBar,
+    Swiper
+  },
+  data () {
+    return {
+      banners: [],
+      recommends: []
+    }
+  },
+  created () {
+    /**
+     * 请求多个数据
+     */
+    getHomeMultidata().then(res => {
+      this.banners = res.data.banner.list
+      this.recommends = res.data.recommend.list
+    })
+  }
 }
 </script>
 
 <style scoped>
-
+  .home-nav{
+    background-color: var(--color-tint);
+    color: white;
+  }
 </style>
