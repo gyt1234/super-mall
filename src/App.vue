0162952d7139cba3f968d3d796826f9cc24fa909
gyt <2509900352@qq.com>
2020-08-09 20:47:09 +0800

    修改文件路径

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
 </template>
 
 <script>
-import MainTabBar from '@/components/MainTabBar'
+import MainTabBar from '@/components/content/mainTabbar/MainTabBar'
 export default {
   data () {
     return {
